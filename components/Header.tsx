e4e327b8b1e0abe45dd0cbe9bbaa13bf54469ac8
nandan vasista <nandan@nandan.local>
2022-11-01 13:35:18 +0530

    added contact page contact with mail and front end for notif

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,54 +1,107 @@
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
+import { MdNotificationsActive } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useState } from "react";
 const Header = () => {
   const [user, loading] = useAuthState(auth);
-
+  const [perm, setPerm] = useState(false);
+  const [showMsg, setShowMsg] = useState(false);
+  const handleClick = () => {
+    Notification.requestPermission().then((perm) => {
+      if (perm === "granted") {
+        setPerm(true);
+        setShowMsg(true);
+        const nofif = new Notification("Following", {
+          data: `Hi ${user?.displayName}`,
+          body: "Thanks For Following Us Pal, Appretiate it",
+        });
+        nofif.addEventListener("close", (e) => {
+          console.log(e);
+        });
+      } else {
+        setPerm(false);
+        setShowMsg(true);
+      }
+    });
+  };
   return (
-    <header className="py-5 px-5 flex justify-between max-w-7xl mx-auto">
-      <div className="flex items-center space-x-5">
-        <Link className="text-3xl w-44 object-contain cursor-pointer" href="/">
-          GeoBlog
-        </Link>
-        <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="text-white bg-orange-600 px-4 py-1 rounded-full">
-            Follow
-          </h3>
-        </div>
-      </div>
-
-      {/* sign in part */}
-      <div className="flex items-center space-x-5 text-orange-600">
-        {!user ? (
-          <>
-            <Link href={"/auth/login"}>
-              <h3>Sign in</h3>
+    <>
+      <header className="py-5 px-5 flex justify-between max-w-7xl mx-auto">
+        <div className="flex items-center space-x-5">
+          <Link
+            className="text-3xl w-44 object-contain cursor-pointer"
+            href="/"
+          >
+            GeoBlog
+          </Link>
+          <div className="hidden md:inline-flex items-center space-x-5">
+            <Link href={"/about"}>
+              <h3>About</h3>
             </Link>
-            <h3 className="border px-4 py-1 rounded-full border-orange-600">
-              Get Started
-            </h3>
-          </>
-        ) : (
-          <>
-            <div className="flex flex-row-reverse items-center space-x-5 text-orange-600">
-              <Link href="/dashboard">
-                <img
-                  src={user.photoURL!}
-                  className="rounded-full w-12 mx-2 md:mx-4 lg:mx-8"
-                  alt="avatar"
-                  referrerPolicy="no-referrer"
-                />
-              </Link>
-              <h3 className="border px-4 py-1 rounded-full border-orange-600">
-                Hi {user.displayName}
+            <Link href={"/contact"}>
+              <h3>Contact</h3>
+            </Link>
+            <div onClick={handleClick}>
+              <h3 className="text-white bg-orange-600 px-4 py-1 rounded-full flex items-center cursor-pointer">
+                Follow
+                <MdNotificationsActive className="inline cursor-pointer" />
               </h3>
             </div>
-          </>
-        )}
-      </div>
-    </header>
+          </div>
+        </div>
+
+        {/* sign in part */}
+        <div className="flex items-center space-x-5 text-orange-600">
+          {!user ? (
+            <>
+              <Link href={"/auth/login"}>
+                <h3>Sign in</h3>
+              </Link>
+              <h3 className="border px-4 py-1 rounded-full border-orange-600">
+                Get Started
+              </h3>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-row-reverse items-center space-x-5 text-orange-600">
+                <Link href="/dashboard">
+                  <img
+                    src={user.photoURL!}
+                    className="rounded-full w-12 mx-2 md:mx-4 lg:mx-8"
+                    alt="avatar"
+                    referrerPolicy="no-referrer"
+                  />
+                </Link>
+                <h3 className="border px-4 py-1 rounded-full border-orange-600">
+                  Hi {user.displayName}
+                </h3>
+              </div>
+            </>
+          )}
+        </div>
+      </header>
+      {perm === true && showMsg === true && (
+        <div className="bg-green-100 flex items-center justify-around text-green-800 text-normal text-center font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+          <h4>
+            Thanks For Following Us. Our Team Will Mail You Once A New Post Is
+            Sent
+          </h4>
+          <button onClick={() => setShowMsg(false)}>
+            <AiFillCloseCircle className="text-orange-400" />
+          </button>
+        </div>
+      )}
+      {perm === false && showMsg === true && (
+        <div className="bg-red-100 flex items-center justify-around text-red-600 text-normal text-center font-semibold mr-2 px-2.5 py-0.5 rounded">
+          <h4>access denied</h4>
+          <button onClick={() => setShowMsg(false)}>
+            <AiFillCloseCircle className="text-orange-500" />
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ pages/contact.jsx
@@ -0,0 +1,89 @@
+import Header from "../components/Header";
+import { useState } from "react";
+const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [body, setBody] = useState("");
+  const handleClick = () => {
+    window.open(
+      `mailto:nandan.vasista.bh@gmail.com?subject=${subject}&body=${body}`
+    );
+  };
+  return (
+    <div>
+      <Header />
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-orange-500 dark:text-orange-700 ">
+            Contact Us
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            Got a technical issue? Want to send feedback about a beta feature?
+            Need details about our Business plan? Let us know.
+          </p>
+          <form action="#" className="space-y-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={() => setEmail(e.target.value)}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
+                placeholder="name@geoblog.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                value={body}
+                onChange={() => setBody(e.target.value)}
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                value={email}
+                onChange={() => setEmail(e.target.value)}
+                id="message"
+                rows="6"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              onClick={handleClick}
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-orange-700 sm:w-fit hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
