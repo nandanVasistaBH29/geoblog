a3b3abc164b04f188830489ad694adc48aeeb478
nandan vasista <nandan@nandan.local>
2022-11-07 18:38:18 +0530

    toUpperCase err

--- pages/news/country/[slug].jsx
+++ pages/news/country/[slug].jsx
@@ -5,9 +5,11 @@ const Country = () => {
   const { slug } = route.query;
   return (
     <div>
-      <h2 className="mt-4 font-bold p-2 text-4xl text-cyan-500 hover:underline md:text-5xl">
-        Trending News In {slug.toUpperCase()}
-      </h2>
+      {slug && (
+        <h2 className="mt-4 font-bold p-2 text-4xl text-cyan-500 hover:underline md:text-5xl">
+          Trending News In {slug.toUpperCase()}
+        </h2>
+      )}
       <TrendingNews countryCode={slug} />
     </div>
   );
