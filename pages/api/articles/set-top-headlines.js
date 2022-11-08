d062418d68f2a9612d04161350d2fd5626414097
nandan vasista <nandan@nandan.local>
2022-11-08 09:38:30 +0530

    test

--- pages/api/articles/set-top-headlines.js
+++ pages/api/articles/set-top-headlines.js
@@ -1,14 +1,18 @@
 import Redis from "ioredis";
 const DEFAULT_EXP = 60 * 60 * 6; //6hrs
-const redisClient = new Redis({ url: process.env.REDIS_URL });
+const redisClient = new Redis(process.env.REDIS_URL);
 redisClient.connect();
 export default async function handler(req, res) {
   const country = req.query.country;
   const articles = req.body.articles;
-
-  redisClient.setex(
-    `articlesByApiTEST6?country=${country}`,
-    DEFAULT_EXP,
-    JSON.stringify(articles)
-  );
+  try {
+    redisClient.setex(
+      `articlesByApiTEST?country=${country}`,
+      DEFAULT_EXP,
+      JSON.stringify(articles)
+    );
+    res.send({ success: "Success" });
+  } catch (err) {
+    res.send({ err });
+  }
 }
